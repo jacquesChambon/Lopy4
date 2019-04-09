b9527c95ed254d172a11e3c463159fa967f16a43
jacquesChambon <48184219+jacquesChambon@users.noreply.github.com>
2019-04-09 22:00:49 +0200

    SSID & passwd set as global variables If not set, user input required

--- WifiSTA/boot.py
+++ WifiSTA/boot.py
@@ -5,11 +5,29 @@ from network import WLAN
 import machine
 wlan = WLAN(mode=WLAN.STA)
 
+SSID=None
+PASS=None
+
+'''
+SSID='iPhone 6S de Jacques'
+PASS='0670726938'
+
+SSID='Toscane'
+PASS='0492420299'
+'''
+
+if not SSID:
+    print('Type SSID : ', end='')
+    SSID=input()
+if not PASS:
+    print('Type PASS : ', end='')
+    PASS=input()
+
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == 'iPhone 6S de Jacques':
+    if net.ssid == SSID:
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, '0670726938'), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, PASS), timeout=5000)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
         print('WLAN connection succeeded!')
