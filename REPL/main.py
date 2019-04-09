41887f1d810f470c80d2c766882811c8d8e85b8c
jacquesChambon <48184219+jacquesChambon@users.noreply.github.com>
2019-04-09 22:03:56 +0200

    Pled demo added

--- REPL/main.py
+++ REPL/main.py
@@ -1,3 +1,11 @@
 """ TOCS example usage """
 
+import pycom, time
+
 print("Hello world")
+
+def pled():
+    pycom.heartbeat(False)
+    pycom.rgbled(0xff00)
+    time.sleep(5)
+    pycom.heartbeat(True)
